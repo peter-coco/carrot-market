d68fb382cd61aefbff086cc709e518c9c536cf04
peter-coco <dklirser@gmail.com>
2022-03-25 18:33:13 +0900

    set-up project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-blue-700">hi test</h1>
+    </div>
+  );
+};
+
+export default Home;
